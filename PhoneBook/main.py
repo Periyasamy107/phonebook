12832675b364f302580b9bba9962fe187a5474cf
Periyasamy10 <cperiyasamy085@gmail.com>
2023-12-14 11:16:20 +0530

    main file modified in some lines of code

--- PhoneBook/main.py
+++ PhoneBook/main.py
@@ -25,8 +25,8 @@ def main():
         CREATE TABLE IF NOT EXISTS phone_records (
             Name VARCHAR(255),
             Email VARCHAR(255),
-            Phone1 INT,
-            Phone2 INT
+            Phone1 VARCHAR(20),
+            Phone2 VARCHAR(20)
         )
     '''
     cursor.execute(create_table_query)
@@ -44,7 +44,6 @@ def main():
                 # Read the file and insert into the database
                 if file.type == "text/csv":
                     df = pd.read_csv(file)
-
                 elif file.type == "application/json":
                     df = pd.read_json(file)
 
@@ -72,8 +71,8 @@ def main():
             elif operation == "Insert Record":
                 name = st.text_input("Name:", "")
                 email = st.text_input("Email:", "")
-                phone1 = st.text_input("Phone1:", "")
-                phone2 = st.text_input("Phone2:", "")
+                phone1 = st.text_input("Phone 1:", "")
+                phone2 = st.text_input("Phone 2:", "")
 
                 if st.button("Insert Record"):
                     insert_single_record_into_db(cursor, name, email, phone1, phone2)
@@ -84,8 +83,8 @@ def main():
                 condition = st.text_input("Enter WHERE condition (e.g., Name='Doe'):", "")
                 name = st.text_input("New Name:", "")
                 email = st.text_input("New Email:", "")
-                phone1 = st.text_input("New Phone1:", "")
-                phone2 = st.text_input("New Phone2:", "")
+                phone1 = st.text_input("New Phone 1:", "")
+                phone2 = st.text_input("New Phone 2:", "")
 
                 if st.button("Update Record"):
                     update_record_in_db(cursor, condition, name, email, phone1, phone2)
@@ -111,13 +110,13 @@ def insert_records_into_db(cursor, df):
     for index, row in df.iterrows():
         cursor.execute('''
             INSERT INTO phone_records (Name, Email, Phone1, Phone2)
-            VALUES (%s, %s, %d, %d)
+            VALUES (%s, %s, %s, %s)
         ''', (row['Name'], row['Email'], row['Phone1'], row['Phone2']))
 
 def insert_single_record_into_db(cursor, Name, Email, Phone1, Phone2):
     cursor.execute('''
         INSERT INTO phone_records (Name, Email, Phone1, Phone2)
-        VALUES (%s, %s, %d, %d)
+        VALUES (%s, %s, %s, %s)
     ''', (Name, Email, Phone1, Phone2))
 
 def select_records_from_db(cursor, condition):
@@ -125,12 +124,12 @@ def select_records_from_db(cursor, condition):
     if condition:
         query += f" WHERE {condition}"
     cursor.execute(query)
-    return pd.DataFrame(cursor.fetchall(), columns=['Name', 'Email', 'Phone1', 'Phone2'])
+    return pd.DataFrame(cursor.fetchall(), columns=['Name', 'Email', 'Phone 1', 'Phone 2'])
 
 def update_record_in_db(cursor, condition, Name, Email, Phone1, Phone2):
     update_query = '''
         UPDATE phone_records
-        SET Name = %s, Email = %s, Phone1 = %d, Phone2 = %d
+        SET Name = %s, Email = %s, Phone1 = %s, Phone2 = %s
         WHERE {}
     '''.format(condition)
     cursor.execute(update_query, (Name, Email, Phone1, Phone2))
